e6d8ea7d1e5fabcc54de45e32b78e711cb47a2cc
nawawn <nawawn@googlemail.com>
2019-08-05 17:58:32 +0100

    script to download or upload to azure blob added

---
+++ SaveAzblob.ps1
@@ -0,0 +1,48 @@
+﻿Function Download-BlobContainer{
+    Param(
+        [Parameter(Mandatory)][String]$ResourceGroup,
+        [Parameter(Mandatory)][String]$StorageAccountName,
+        [Parameter(Mandatory)][String]$ContainerName,
+        [AllowNull()][String]$FileName,
+        [String]$Destination = $(Get-Location)
+    )
+    Begin{
+        $Storage = Get-AzStorageAccount -ResourceGroup $ResourceGroup -StorageAccountName $StorageAccountName
+    }
+    Process{
+        $Blob = Get-AzStorageBlob -Container $ContainerName -Context $Storage.Context
+        If ($FileName){
+            Get-AzStorageBlobContent -Blob $FileName -Container $ContainerName -Destination $Destination -Context $Storage.Context
+        }
+        Else{
+            Foreach($file in $Blob){
+                Get-AzStorageBlobContent -Blob $File.Name -Container $ContainerName -Destination $Destination -Context $Storage.Context           
+            }    
+        }
+    }
+<#
+.Example
+    Download-BlobContainer -ResourceGroup 'RG-Storage' -StorageAccountName 'stortest012' -ContainerName 'files-container'
+#>
+}
+
+Function UploadTo-BlobContainer{
+    Param(
+        [Parameter(Mandatory)][String]$ResourceGroup,
+        [Parameter(Mandatory)][String]$StorageAccountName,
+        [Parameter(Mandatory)][String]$ContainerName,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [String[]]$File
+        
+    )
+    Begin{        
+        $Storage = Get-AzStorageAccount -ResourceGroup $ResourceGroup -StorageAccountName $StorageAccountName
+    }
+    Process{        
+        Set-AzStorageBlobContent -File $FileName -Container $containerName -Blob $((Get-Item $File).Name) -Context $Storage.Context
+    }
+<#
+.EXAMPLE
+   UploadTo-BlobContainer -ResourceGroup 'RG-Storage' -StorageAccountName 'stortest012' -ContainerName 'files-container' -File 'C:\temp\test.txt'
+#>
+}
